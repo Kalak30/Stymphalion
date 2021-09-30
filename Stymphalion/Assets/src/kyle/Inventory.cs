6e30284a842c87f5968de018b8951791887e9d6c
Kyle-1213 <81591970+Kyle-1213@users.noreply.github.com>
2021-09-29 21:16:07 -0700

    Updating inventory file

--- Stymphalion/Assets/src/kyle/Inventory.cs
+++ Stymphalion/Assets/src/kyle/Inventory.cs
@@ -10,7 +10,7 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        inventoryAction = gameObject.AddComponent<Inventory>()as Inventory;
+       
     }
 
     // Update is called once per frame
@@ -19,11 +19,9 @@ public class Inventory : MonoBehaviour
         
     }
 
-    public void initializeBattle(){
+    public void InventoryCreation(){
 
         Debug.Log("Inventory not yet set up");
         Debug.Log("Inventory will call function from Inventory shown below");
-        inventoryAction.InventoryCreation();
-
     }
 }
