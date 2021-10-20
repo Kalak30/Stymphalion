ea8a2b0fae8aba6072ec9acdee1f62ad044cd0a0
Hunter-SE <90203827+Hunter-SE@users.noreply.github.com>
2021-10-20 08:48:09 -0700

    Refactored scripts
    Updated my current scripts to match our coding standards

--- Stymphalion/Assets/src/hunter/Menus Assets/MMBGM.cs
+++ Stymphalion/Assets/src/hunter/Menus Assets/MMBGM.cs
@@ -1,12 +1,27 @@
+/*
+* Filename: MMBGM.cs
+* Developer: Hunter Leppek
+* Purpose: This file implements control of the main menu background music (MMBGM)
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+/// <summary>
+/// A class that implements control of the MMGBM
+/// Member Variables
+/// <list type = "bullet">
+/// <item>m_volume_slider</item>
+/// </list>
+/// </summary>
 public class MMBGM : MonoBehaviour
 {
-    [SerializeField] Slider volumeSlider;
-    // Start is called before the first frame update
+    [SerializeField] Slider m_volume_slider;
+    /// <summary>
+    /// Initializes the volume slider
+    /// </summary>
     void Start()
     {
         if(!PlayerPrefs.HasKey("musicVolume"))
@@ -20,19 +35,28 @@ public class MMBGM : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Changes the volume in accordance to the value of the volume slider
+    /// </summary>
     public void changeVolume()
     {
-        AudioListener.volume = volumeSlider.value;
+        AudioListener.volume = m_volume_slider.value;
         Save();
     }
 
-    private void Load()
-    {
-        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
-    }
-
+    /// <summary>
+    /// Saves the value of the volume slider so that the player does not have to set the volume everytime they enter the game
+    /// </summary>
     private void Save()
     {
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        PlayerPrefs.SetFloat("musicVolume", m_volume_slider.value);
+    }
+
+    /// <summary>
+    /// Loads the saved value of the volume slider
+    /// </summary>
+    private void Load()
+    {
+        m_volume_slider.value = PlayerPrefs.GetFloat("musicVolume");
     }
 }
