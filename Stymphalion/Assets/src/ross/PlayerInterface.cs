5e009bd0c5992d79e74ff7590f6e4b21d2fe52f7
RCP-2001 <coalgolem.xd@gmail.com>
2021-10-20 17:30:30 -0700

    PlayerClass Changes

---
+++ Stymphalion/Assets/src/ross/PlayerInterface.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInterface : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  gameObject.find("UI_Invetory")
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
