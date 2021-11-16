e10b2d637ccd11979a634a8c279f7a85a53c2338
Hunter-SE <90203827+Hunter-SE@users.noreply.github.com>
2021-11-16 11:09:31 -0800

    Pause Menu

--- Stymphalion/Assets/src/hunter/Menus Assets/MainMenu.cs
+++ Stymphalion/Assets/src/hunter/Menus Assets/MainMenu.cs
@@ -1,7 +1,7 @@
 /*
 * Filename: MainMenu.cs
 * Developer: Hunter Leppek
-* Purpose: This file implements a main menu for the game
+* Purpose: This file implements a main menu for the game. 
 */
 using System.Collections;
 using System.Collections.Generic;
@@ -10,14 +10,17 @@ using UnityEngine.SceneManagement;
 
 
 /// <summary>
-/// A main menu class for the main menu
+/// A main menu class for the main menu. This acts as a superclass to the pause menu.
 /// </summary>
 public class MainMenu : MonoBehaviour
 {
     /// <summary>
     /// A function to change the scene to the game once the main menu play button is pressed.
+    /// This function is virtual because the main menu and the pause menu function differ
+    /// The main menu PlayGame changes scene, whereas the pause menu play game resumes time and hides itself.
+    /// This is a case of dynamic binding.
     /// </summary>
-    public void PlayGame()
+    public virtual void PlayGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
