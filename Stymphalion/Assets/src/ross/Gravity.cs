2b4811022af60d68c39d933f5ad83177f6441d47
RCP-2001 <coalgolem.xd@gmail.com>
2021-11-15 18:42:18 -0800

    Added Hydra to hydra cave + Scene Sets Gravity now

---
+++ Stymphalion/Assets/src/ross/Gravity.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity = Vector3.zero;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
