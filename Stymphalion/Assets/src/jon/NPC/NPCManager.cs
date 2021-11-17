5fddcad1e51de3869ac2a1d13682e505e3127934
Kalak30 <kopfbros2@gmail.com>
2021-11-16 20:52:33 -0800

    shop NPC shows the shop

--- Stymphalion/Assets/src/jon/NPC/NPCManager.cs
+++ Stymphalion/Assets/src/jon/NPC/NPCManager.cs
@@ -26,6 +26,7 @@ public class NPCManager : MonoBehaviour
     {
         m_npcs = new List<NPC>();
         AddQuestNPC(1);
+        AddShopNPC(null);
     }
 
     public void Update()
@@ -54,6 +55,15 @@ public class NPCManager : MonoBehaviour
         QuestNPC npc = npc_obj.GetComponent<QuestNPC>();
         npc.transform.position = new Vector3(28,-4, 0);
         npc.m_npc_quest = QuestManager.GetQuestManager().GetQuest(quest_no);
+        m_npcs.Add(npc);
+    }
+
+    public void AddShopNPC(List<Item.ItemType> available_items)
+    {
+        GameObject shop_obj = GameObject.Instantiate(m_shopnpc_prefab);
+        ShopNPC npc = shop_obj.GetComponent<ShopNPC>();
+        npc.transform.position = new Vector3(28, -6, 0);
+
         m_npcs.Add(npc);
     }
 }
