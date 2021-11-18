bdb0f5dbcc87891ed30596ec83ebc02bd53694ee
RCP-2001 <coalgolem.xd@gmail.com>
2021-11-17 21:43:28 -0800

    a lil documentation

--- Stymphalion/Assets/src/ross/Gravity.cs
+++ Stymphalion/Assets/src/ross/Gravity.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Gravity : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    /// <summary>
+    /// Do No Gravity In Over world
+    /// </summary>
     void Start()
     {
         Physics.gravity = Vector3.zero;
